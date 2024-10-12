9f831fd3dad2bd2868f61672b10872df79c0b729
shrudh-i <shrudhi.rs21@gmail.com>
2024-10-12 16:53:04 -0400

    working dqn.py

--- F24_703HW2/code/dqn/dqn_old2.py
+++ F24_703HW2/code/dqn/dqn_old2.py
@@ -15,7 +15,9 @@ import torch.optim as optim
 # we added these
 import copy
 
-Transition = collections.namedtuple("Transition", field_names=['state', 'action', 'reward', 'next_state', 'dones'])
+
+
+Transition = collections.namedtuple("Transition", field_names=['state', 'action', 'reward', 'next_state'])
 
 class ReplayMemory():
     def __init__(self, memory_size, batch_size):
@@ -24,20 +26,45 @@ class ReplayMemory():
         # BEGIN STUDENT SOLUTION
         self.memory = collections.deque(maxlen=memory_size)
         self.batch_size = batch_size
+        self.selected_indices = set()
         # END STUDENT SOLUTION
 
     def sample(self):
-        if len(self.memory) < self.batch_size:
-            print('ERROR: Not enough samples in replay buffer')
-            return random.sample(self.memory, len(self.memory))
-        
         return random.sample(self.memory, self.batch_size)
+        # Convert deque to a list
+        memory_list = list(self.memory)
+        
+        # Get available indices that haven't been selected
+        available_indices = list(set(range(len(memory_list))) - self.selected_indices)
+
+        if len(available_indices) < self.batch_size:
+            raise ValueError("Not enough elements left to sample.")
+
+        # Sample without replacement from available indices
+        sampled_indices = random.sample(available_indices, self.batch_size)
+
+        # Mark these indices as selected
+        self.selected_indices.update(sampled_indices)
+
+        # Return the sampled elements
+        return [memory_list[i] for i in sampled_indices]
+
+
+    # def sample_batch(self):
+        # randomly chooses from the collections.deque
+        # BEGIN STUDENT SOLUTION
+        # self.replay_buffer.sample
+        # samples = np.random.choice(len(self.memory), self.batch_size, replace=True)
+        # batch = zip(*[self.memory[i] for i in samples])
+        # batch = Transition(*zip(*t))
+        # return batch
+        # END STUDENT SOLUTION
 
 
     def append(self, transition):
         # append to the collections.deque
         # BEGIN STUDENT SOLUTION
-        self.memory.append(transition)
+        self.memory.append(np.array(transition, dtype=object))
         # END STUDENT SOLUTION
 
 
@@ -59,6 +86,8 @@ class DeepQNetwork(nn.Module):
 
         self.device = device
 
+        # self.loss = nn.functional.mse_loss()
+
         hidden_layer_size = 256
 
         # q network
@@ -77,22 +106,20 @@ class DeepQNetwork(nn.Module):
 
         # initialize network and optimizer
         self.q_omega = q_net_init()
-        self.q_target = copy.deepcopy(q_net_init()) 
+        self.q_target = q_net_init() #copy.deepcopy(self.q_omega)
         self.q_target.load_state_dict(self.q_omega.state_dict()) 
-        self.q_target.eval()
 
         self.optimizer = optim.Adam(self.q_omega.parameters(), lr_q_net)
         # END STUDENT SOLUTION
 
     def burn_traj(self, env):
         state, _ = env.reset()
-        for _ in range(self.burn_in):
-            action = env.action_space.sample()
+        for i in range(self.burn_in):
+            action = env.action_space.sample()#np.random.choice([0,1])
 
             next_state, reward, done, _, _ = env.step(action)
 
-            self.replay_buffer.append(Transition(torch.from_numpy(state).float(), action, reward, next_state, done))
-            
+            self.replay_buffer.append(Transition(torch.from_numpy(state).float(), action, reward, next_state))
             if done:
                 state, _ = env.reset()
             else:
@@ -106,15 +133,24 @@ class DeepQNetwork(nn.Module):
     def get_action(self, state, stochastic):
         # if stochastic, sample using epsilon greedy, else get the argmax
         # BEGIN STUDENT SOLUTION
+        # print(state)
+        # print(torch.from_numpy(state).float())
         with torch.no_grad():
             omega_val, target_val = self.forward(torch.from_numpy(state))
+        if stochastic:
+            # e-greedy
+            sample = random.random()
+            if sample > self.epsilon:
+                # arg-max
+                action = torch.argmax(omega_val)
+            else:
+                action = torch.randint(0,1, (1,))
 
-        if stochastic and random.random() < self.epsilon:
-            return random.randint(0, self.action_size - 1)
         else:
-            return omega_val.argmax().item()
-
+            # arg-max
+            action = torch.argmax(target_val)
         # END STUDENT SOLUTION
+        return action.item()
 
 
 
@@ -122,110 +158,82 @@ class DeepQNetwork(nn.Module):
         # train the agent using the replay buffer
         # BEGIN STUDENT SOLUTION
         # minibatch= self.replay_buffer.sample_batch()
-        if len(self.replay_buffer.memory) < self.replay_buffer.batch_size:
-            return
-        
         trans = self.replay_buffer.sample()
-
         minibatch = Transition(*zip(*trans))
 
-        # non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-        #                                   minibatch.next_state)), dtype=torch.bool)
-        
-        # non_final_next_states = np.array([s for s in minibatch.next_state if s is not None])
-       
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          minibatch.next_state)), dtype=torch.bool)
+        # print(np.shape(minibatch.next_state))
+        non_final_next_states = np.array([s for s in minibatch.next_state if s is not None])
+        # print(np.shape(non_final_next_states))
 
         state_batch = torch.tensor(np.array(minibatch.state))
+        # print("Minibatch state", np.shape(state_batch))
+        # print(minibatch.action)
         
         action_batch = torch.tensor(minibatch.action)
+        # print(np.shape(action_batch))
 
         reward_batch = torch.tensor(minibatch.reward)
+        # print(state_batch)
 
         state_action_values = self.q_omega(state_batch).gather(1, action_batch.unsqueeze(1))
 
-        dones = torch.tensor(np.array(minibatch.dones), dtype=torch.float32, device=self.device)
+        next_state_values = torch.zeros(self.replay_buffer.batch_size)
 
-        # next_state_values = torch.zeros(self.replay_buffer.batch_size)
-
-        # next_state_values[non_final_mask] = self.q_target(torch.from_numpy(non_final_next_states)).max(1).values
-        next_state_values = self.q_target(torch.from_numpy(np.array(minibatch.next_state))).max(1).values
-        # # with torch.no_grad():
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.q_target(torch.from_numpy(non_final_next_states)).max(1).values
         
-        # # Compute the expected Q values
-        expected_state_action_values = (next_state_values * self.gamma * (1-dones)) + reward_batch
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
         
-        # # loss_omega = self.loss(state_action_values.squeeze(1), expected_state_action_values)
-        loss_omega = F.mse_loss(state_action_values.squeeze(1), expected_state_action_values)
+        # print("state action values",state_action_values.squeeze(1))
+        # print("expected state ",expected_state_action_values)
+        # loss_omega = self.loss(state_action_values.squeeze(1), expected_state_action_values)
+        loss_omega = nn.functional.mse_loss(state_action_values.squeeze(1), expected_state_action_values)
 
         self.optimizer.zero_grad()
         loss_omega.backward()
 
         self.optimizer.step()
-        ##################################################################################################
-        # if len(self.replay_buffer.memory) < self.replay_buffer.batch_size:
-        #     return
 
-        # batch = self.replay_buffer.sample()
-        # minibatch = Transition(*zip(*batch))
-
-        # states = minibatch.state
-        # actions = minibatch.action
-        # rewards = minibatch.reward
-        # next_states = minibatch.next_state
-        # dones = minibatch.dones
-
-
-        # states = torch.tensor(np.array(states), dtype=torch.float32, device=self.device)
-        # actions = torch.tensor(actions, dtype=torch.long, device=self.device).unsqueeze(1)
-        # rewards = torch.tensor(rewards, dtype=torch.float32, device=self.device)
-        # next_states = torch.tensor(np.array(next_states), dtype=torch.float32, device=self.device)
-        # dones = torch.tensor(dones, dtype=torch.float32, device=self.device)
-
-        # q_values = self.q_omega(states).gather(1, actions).squeeze()
-        # next_q_values = self.q_target(next_states).max(1)[0]
-        # target_q_values = rewards + self.gamma * next_q_values * (1 - dones)
-
-        # loss = F.mse_loss(q_values, target_q_values)
-        # self.optimizer.zero_grad()
-        # loss.backward()
-        # self.optimizer.step()
+        # print(minibatch.state)
+        # print("Next state with final state", minibatch.next_state)
+        # non_final_mask = tuple(map(lambda s: s is not None, minibatch.next_state))
+        # non_final_next_states = np.concatenate([s for s in minibatch.next_state if s is not None])
+        # print("State",np.concatenate(minibatch.state))
+        # print("Reward",minibatch.reward)
+        # print("Action",minibatch.action)
+        # print("Next state without final state", non_final_next_states)
+        # print(minibatch)
+        # END STUDENT SOLUTION
+        
 
 
     def run(self, env, max_steps, num_episodes, train, init_buffer):
         total_rewards = []
 
-        if train:
-            self.q_omega.train()
-        else:
-            self.q_omega.eval()
-
         # initialize replay buffer
         # run the agent through the environment num_episodes times for at most max steps
         # BEGIN STUDENT SOLUTION
-        if init_buffer:
-            self.burn_traj(env)
-
+        self.burn_traj(env)
         c = 0
         for e in range(num_episodes):
             state, _ = env.reset()
-            total_reward = 0
             for t in range(max_steps):
                 #get the action
                 action = self.get_action(state,stochastic=True)
                 next_state, reward, done, _, _ = env.step(action)
-                total_reward += reward
-                # if done:
-                #     next_state = None
-                # else:
-                #     state = next_state
+                
+                if done:
+                    next_state = None
+                else:
+                    state = next_state
 
-                self.replay_buffer.append(Transition(torch.from_numpy(state).float(), action, reward, next_state, done))
+                self.replay_buffer.append(Transition(torch.from_numpy(state).float(), action, reward, next_state))
 
                 if train:
                     self.train()
-
-                if not done:
-                    state = next_state
 
                 c = c + 1
 
@@ -236,20 +244,19 @@ class DeepQNetwork(nn.Module):
 
                 if done:
                     break
-            
-            print(f"Episode {e} - Reward {total_reward}")
-            # if (e+1) % 100 == 0:
-            #     cumulative_reward = 0
-            #     for _ in range(20):
-            #         state, _ = env.reset()
-            #         for i in range(max_steps):
-            #             action = self.get_action(state, stochastic=False)
-            #             next_state, reward, done, _, _ = env.step(action)
-            #             cumulative_reward = cumulative_reward + reward
-            #             if done:
-            #                 break
-            #     total_rewards.append(cumulative_reward/20)
-            #     print("Epsodic reward in eval",cumulative_reward/20)
+
+            if (e+1) % 100 == 0:
+                cumulative_reward = 0
+                for _ in range(20):
+                    state, _ = env.reset()
+                    for i in range(max_steps):
+                        action = self.get_action(state, stochastic=False)
+                        next_state, reward, done, _, _ = env.step(action)
+                        cumulative_reward = cumulative_reward + reward
+                        if done:
+                            break
+                total_rewards.append(cumulative_reward/20)
+                print("Epsodic reward in eval",cumulative_reward/20)
         # END STUDENT SOLUTION
         return(total_rewards)
 
@@ -301,7 +308,7 @@ def main():
 
     for run in range(num_runs):
         q_net = DeepQNetwork(env.observation_space.shape[0], env.action_space.n)
-        rewards = q_net.run(env, max_steps, num_episodes, 1, 1)
+        rewards = q_net.run(env, max_steps, num_episodes, 1, 0)
         run_total_rewards[run] = rewards
 
     graph_agents("I dunno", 0, 0, max_steps= max_steps, num_episodes = num_episodes, total_rewards=run_total_rewards)
